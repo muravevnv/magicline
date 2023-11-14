e3f37e753164ecd4fed967a2fad5217a50513044
Nickolay Muravev <murvievnick@gmail.com>
2023-11-15 00:51:53 +0300

    markup pt.3

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -12,5 +12,21 @@ function closeModal() {
 
 $('.js-open-modal').on('click', openModal)
 
-$('.modal-overlay').on('click', closeModal)
-$('.modal-close').on('click', closeModal)
+$('.modal-overlay').on('click', closeModal);
+$('.modal-close').on('click', closeModal);
+
+
+$('[data-role="tab"]').each(function(){
+  let tabs = $(this).find('.tabs-item');
+  let tabsContent = $(this).find('.tabs-content');
+
+  tabs.on('click', function(){
+    let dataTab = $(this).data('tab');
+
+    tabs.removeClass('is-active');
+    $(this).addClass('is-active');
+    
+    tabsContent.removeClass('is-open');
+    $(`.tabs-content[data-tab="${dataTab}"]`).addClass('is-open');
+  })
+})
