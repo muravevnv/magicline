458d2eb66e38be5af1ecfca8c8bd96d8622d555c
Nickolay Muravev <murvievnick@gmail.com>
2023-11-16 00:16:34 +0300

    add 404, fix links in sitemap

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -59,3 +59,28 @@ $('.header-burger').on('click', function(){
   $('.header').toggleClass('is-active')
   $('.nav').toggleClass('is-open');
 })
+
+if (window.matchMedia("(min-width: 1024px)").matches) {
+  $(window).on("mousemove", function (e) {
+    var w = $(window).width();
+    var h = $(window).height();
+    var offsetX = 0.5 - e.pageX / w;
+    var offsetY = 0.5 - e.pageY / h;
+
+    $(".parallax-layer").each(function (i, el) {
+      var offset = parseInt($(el).data("speed"));
+      var translate =
+        "translate3d(" +
+        Math.round(offsetX * offset) +
+        "px," +
+        Math.round(offsetY * offset) +
+        "px, 0px)";
+
+      $(el).css({
+        "-webkit-transform": translate,
+        transform: translate,
+        "moz-transform": translate,
+      });
+    });
+  });
+}
